a04d4d82e08459b6d7bbd9858b6b254f953356b3
Bar Harel <bzvi7919@gmail.com>
2018-06-12 20:38:02 +0300

    Need to solve the weakrefs on this one

--- bdict/__init__.py
+++ bdict/__init__.py
@@ -1,19 +1,52 @@
+"""A library allowing you to create an auto method-binding dict.
+
+Mainly used for event handlers, a binding dict consists of a mapping between
+any events or keys, to their appropriate handler functions within a class.
+Upon key lookup, the dict will bind the appropriate function to the instance
+of the class.
+
+For an example:
+
+class Server:
+    def __init__(self, name):
+        self.name = name
+    def on_connect(self, remote_host):
+        print(self.name, remote_host)
+    def on_connect(self, remote_host):
+        print(self.name, remote_host)
+    handlers = BDict({NewConnectionEvent: on_connect,
+    DisconnectonEven: on_disconnect})
+
+>>> s = Server("myserver")
+>>> s.handlers[NewConnectionEvent]("1.2.3.4")
+myserver 1.2.3.4
+
+As you can see, after accessing the handlers dict, and upon key lookup,
+the dict bound the handler functions to the instance.
+"""
 __author__ = "Bar Harel"
 __version__ = "0.1.0b"
 __license__ = "MIT"
 __all__ = ["BDict"]
 
-from typing import (Callable as _Callable, Mapping as _Mapping,
-                    Dict as _Dict, cast as _cast,
-                    TypeVar as _TypeVar, Any as _Any)
-from weakref import ref as _ref
 
+from typing import (Callable as _Callable, Mapping as _Mapping,
+                    Dict as _Dict, cast as _cast, Union as _Union,
+                    Iterable as _Iterable, Tuple as _Tuple,
+                    MutableMapping as _MutableMapping, overload as _overload,
+                    TypeVar as _TypeVar, Any as _Any, Type as _Type)
+from weakref import ref as _ref, WeakKeyDictionary as _WeakKeyDictionary
+
+
+_T = _TypeVar("_T")
 _KT = _TypeVar("_KT")
 _VT = _TypeVar("_VT", bound=_Callable)
+BDICT_INPUT_TYPE = _Union[_Iterable[_Tuple[_KT, _VT]], _Mapping[_KT, _VT]]
 
 
 class BDict(_Dict[_KT, _VT]):
-    __slots__ = ("name", "strong")
+    """An auto method-binding dict"""
+    __slots__ = ("name", "strong", "_instance_dicts")
 
     class BoundDict(_Dict):
         __slots__ = ("_inst", "external")
@@ -71,9 +104,48 @@ class BDict(_Dict[_KT, _VT]):
                 self.external: _Dict = {}
                 self.external[key] = value
 
-    def __init__(self, dict_: _Mapping[_KT, _VT], *,
+        def __delitem__(self, key: _Any) -> None:
+            super().__delitem__(key)
+            self.external.pop(key, None)
+
+    class ClassBoundDict(_MutableMapping):
+        __slots__ = ("bdict", "owner")
+
+        def __init__(self, bdict, owner):
+            self.bdict = bdict
+            self.owner = owner
+
+        def __getitem__(self, key):
+            return self.bdict[key].__get__(None, self.owner)
+
+        def __setitem__(self, key, value):
+            self.bdict[key] = value
+
+        def __delitem__(self, key):
+            del self.bdict[key]
+
+        def __iter__(self):
+            return iter(self.bdict)
+
+        def __len__(self):
+            return len(self.bdict)
+
+    def __init__(self, dict_: BDICT_INPUT_TYPE[_KT, _VT], *,
                  strong: bool=None) -> None:
-        super().__init__(dict_)
+        """Initialize the auto method-binding dict
+
+        Args:
+            dict: Any mapping between keys to class functions
+            strong: Whether to strongly reference instances upon dict
+            creation or only weakly reference. By default, a weak reference
+            will be created in order to avoid a reference cycle. Unless forced
+            using 'strong=False', in case of a class which defines __slots__,
+            the dict will be created with a strong reference as __slots__
+            create caching issues.
+            Reference to class'es BDict will always be strongly referenced.
+        """
+        super().__init__(_cast(_Mapping[_KT, _VT], dict_))
+        self._
         self.strong = strong
 
     def __repr__(self):
@@ -83,6 +155,17 @@ class BDict(_Dict[_KT, _VT]):
     def __set_name__(self, owner, name):
         self.name = name
 
+    def __getitem__(self, name):
+        key = 
+
+    @_overload
+    def __get__(self, inst: None, owner: _Type) -> ClassBoundDict:
+        ...
+
+    @_overload
+    def __get__(self, inst: _T, owner: _Type[_T]) -> BoundDict:
+        ...
+
     def __get__(self, inst, owner,
                 BoundDict=BoundDict, bind=BoundDict.bind):
 
@@ -90,6 +173,11 @@ class BDict(_Dict[_KT, _VT]):
             return self
 
         strong = self.strong
+
+        bdict = self._instance_dicts.get(inst)
+
+        if bdict is not None:
+            return bdict
 
         bdict = BoundDict(self)
 
