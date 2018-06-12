e538774c764d98fc0d6bcd8b2c87d52464b2e8d8
Bar Harel <bzvi7919@gmail.com>
2018-06-13 00:07:57 +0300

    solved? kinda

--- bdict/__init__.py
+++ bdict/__init__.py
@@ -33,6 +33,7 @@ __all__ = ["BDict"]
 from typing import (Callable as _Callable, Mapping as _Mapping,
                     Dict as _Dict, cast as _cast, Union as _Union,
                     Iterable as _Iterable, Tuple as _Tuple,
+                    Optional as _Optional,
                     MutableMapping as _MutableMapping, overload as _overload,
                     TypeVar as _TypeVar, Any as _Any, Type as _Type)
 from weakref import ref as _ref, WeakKeyDictionary as _WeakKeyDictionary
@@ -46,10 +47,15 @@ BDICT_INPUT_TYPE = _Union[_Iterable[_Tuple[_KT, _VT]], _Mapping[_KT, _VT]]
 
 class BDict(_Dict[_KT, _VT]):
     """An auto method-binding dict"""
-    __slots__ = ("name", "strong", "_instance_dicts")
+    __slots__ = ("name", "strong", "_instance_externals", "autocache")
+    strong: _Optional[bool]
+    name: str
+    _instance_externals: _WeakKeyDictionary
 
     class BoundDict(_Dict):
         __slots__ = ("_inst", "external")
+        external: _Dict
+
         _sentinel = object()
 
         def __repr__(self):
@@ -62,14 +68,17 @@ class BDict(_Dict[_KT, _VT]):
                 return (f"<Unbound {self.__class__.__name__} "
                         f"mapping to {super().__repr__()}>")
 
-            return (f"<{self.__class__.__name__} bound to {repr(inst)} "
+            return (f"<{self.__class__.__name__} bound to {inst!r} "
                     f"with the following mapping: {super().__repr__()}>")
 
-        def bind(self, inst: _Any, *, strong: bool=False) -> None:
-
+        def bind(self, inst: _Any, *,
+                 external: _Dict = None, strong: bool = False) -> None:
             # inst=None is special
             if inst is None:
                 raise ValueError("Must bind to an instance.")
+
+            if external is not None:
+                self.external = external
 
             if strong:
                 self._inst = lambda: inst
@@ -77,7 +86,14 @@ class BDict(_Dict[_KT, _VT]):
                 self._inst = _ref(inst)
 
         def __getitem__(self, key: _Any, *, _sentinel=_sentinel) -> _Any:
-            func = super().__getitem__(key)
+            try:
+                func = super().__getitem__(key)
+            except KeyError:
+                external = getattr(self, "external", None)
+                if not external or key not in external:
+                    raise
+                super().__setitem__(key, _sentinel)
+                func = _sentinel
 
             if func is _sentinel:
                 return self.external[key]
@@ -95,13 +111,18 @@ class BDict(_Dict[_KT, _VT]):
 
             return func.__get__(inst, type(inst))
 
-        def __setitem__(self, key: _Any, value: _Any) -> None:
-            super().__setitem__(key, self._sentinel)
+        def __setitem__(self, key: _Any, value: _Any, *,
+                        _sentinel=_sentinel) -> None:
+            # Still initializing (pre-bind)
+            if self._inst is None:
+                super().__setitem__(key, value)
+
+            super().__setitem__(key, _sentinel)
 
             try:
                 self.external[key] = value
             except AttributeError:
-                self.external: _Dict = {}
+                self.external = {}
                 self.external[key] = value
 
         def __delitem__(self, key: _Any) -> None:
@@ -109,11 +130,15 @@ class BDict(_Dict[_KT, _VT]):
             self.external.pop(key, None)
 
     class ClassBoundDict(_MutableMapping):
+        """Temporary proxy bound to the original class"""
         __slots__ = ("bdict", "owner")
 
         def __init__(self, bdict, owner):
             self.bdict = bdict
             self.owner = owner
+
+        def __repr__(self):
+            return f"<classbound proxy to {self.bdict!r}>"
 
         def __getitem__(self, key):
             return self.bdict[key].__get__(None, self.owner)
@@ -131,7 +156,7 @@ class BDict(_Dict[_KT, _VT]):
             return len(self.bdict)
 
     def __init__(self, dict_: BDICT_INPUT_TYPE[_KT, _VT], *,
-                 strong: bool=None) -> None:
+                 strong: bool = None, autocache: bool = True) -> None:
         """Initialize the auto method-binding dict
 
         Args:
@@ -143,10 +168,13 @@ class BDict(_Dict[_KT, _VT]):
             the dict will be created with a strong reference as __slots__
             create caching issues.
             Reference to class'es BDict will always be strongly referenced.
+            autocache: Upon access, will automatically cache the BoundDict
+            on the instance. On classes with __slots__ it's suggested to
+            turn this off as it will not work either case.
         """
         super().__init__(_cast(_Mapping[_KT, _VT], dict_))
-        self._
         self.strong = strong
+        self.autocache = autocache
 
     def __repr__(self):
         return (f"<Autobinding {self.__class__.__name__} "
@@ -154,9 +182,6 @@ class BDict(_Dict[_KT, _VT]):
 
     def __set_name__(self, owner, name):
         self.name = name
-
-    def __getitem__(self, name):
-        key = 
 
     @_overload
     def __get__(self, inst: None, owner: _Type) -> ClassBoundDict:
@@ -170,25 +195,71 @@ class BDict(_Dict[_KT, _VT]):
                 BoundDict=BoundDict, bind=BoundDict.bind):
 
         if inst is None:
-            return self
+            return self.ClassBoundDict(self, owner)
 
         strong = self.strong
 
-        bdict = self._instance_dicts.get(inst)
-
-        if bdict is not None:
-            return bdict
-
+        # Keep in mind it also copies the dict!
         bdict = BoundDict(self)
 
-        try:
-            setattr(inst, self.name, bdict)
-            if strong is None: strong = False
+        # Check if instance externals already exist for this instance
+        externals = getattr(self, "_instance_externals", None)
+        if externals and inst in externals:
+            # Shortcut - Bind straight away and send off
+            bind(bdict, strong=True if strong is None else strong,
+                 external=externals[inst])
+            return bdict
 
-        # Class implemented __slots__
+        try:
+            # If autocache is enabled
+            if self.autocache:
+
+                # Attempt to bind to instance
+                setattr(inst, self.name, bdict)
+
+                # Instance bound, no need for external dict
+                use_external = False
+
+            # Must use external as we need to store the dict's
+            # changes externally (dict is not located on instance)
+            else:
+                use_external = True
+
+        # Class implemented __slots__, failed binding to instance
         except AttributeError:
+            # Must use external (as we cannot autocache).
+            use_external = True
+
+        if use_external:
+            # Should attempt to strong ref in order not to lose the external
+            # dict
             if strong is None: strong = True
 
-        bind(bdict, inst, strong=strong)
+            # Create _instance_externals if one does not exist
+            if externals is None:
+                externals = self._instance_externals = _WeakKeyDictionary()
+
+            # Get the appropriate external and bind
+            external: _Dict
+
+            try:
+                # Keep in mind - local variable "external" is set even if
+                # TypeError is thrown.
+                external = externals[inst] = {}
+            except TypeError:
+                # Instance has __slots__ without __weakref__
+                import warnings
+                msg = (
+                    f"Cannot create a weak reference to "
+                    f"'{inst.__class__.__name__}' object. Changes to BoundDict"
+                    f" will not be shared accross the instance!")
+                warnings.warn(msg)
+
+            bind(bdict, inst, strong=strong, external=external)
+
+        else:
+            # Weakref is fine
+            if strong is None: strong = False
+            bind(bdict, inst, strong=strong)
 
         return bdict
